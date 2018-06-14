7f31716876f09c0204b0cff51c602bd06849d1a8
octaguzz <octaguzz@outlook.com>
2018-06-14 22:36:31 +0100

    Adicionar arquivos de projeto.

---
+++ Livro C-Sharp - Capitulo 8 (exercicios parte 2)/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Livro_C_Sharp___Capitulo_8__exercicios_parte_2_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(199, 238);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(228, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(362, 238);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "disk vector.svg");
+            this.imageList1.Images.SetKeyName(1, "Folder vector.svg");
+            this.imageList1.Images.SetKeyName(2, "file vector.svg");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 304);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ Livro C-Sharp - Capitulo 8 (exercicios parte 2)/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Livro_C_Sharp___Capitulo_8__exercicios_parte_2_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            try
+            {
+                listView1.Items.Clear();
+                if (treeView1.SelectedNode.Text != "C:")
+                {
+                    DirectoryInfo dirC = new DirectoryInfo(@"C:\" + treeView1.SelectedNode.Text);
+                    DirectoryInfo[] subDirs = dirC.GetDirectories();
+                    foreach (DirectoryInfo dir in subDirs)
+                    {
+                        if ((dir.Attributes & FileAttributes.Hidden) != FileAttributes.Hidden)
+                        {
+                            ListViewItem itemDir = listView1.Items.Add(dir.Name);
+                            itemDir.ImageIndex = 1;
+                        }
+
+                    }
+                    FileInfo[] fichs = dirC.GetFiles();
+                    foreach (FileInfo fich in fichs)
+                    {
+                        if ((fich.Attributes & FileAttributes.Hidden) != FileAttributes.Hidden)
+                        {
+                            ListViewItem itemFich = listView1.Items.Add(fich.Name);
+                            itemFich.ImageIndex = 2;
+                        }
+                    }
+                }
+            }
+
+            catch
+            {
+                //caso excepção ao nivel de acesso
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TreeNode root = treeView1.Nodes.Add("C:");
+            root.ImageIndex = 0;
+            DirectoryInfo dirC = new DirectoryInfo("c:/");
+            DirectoryInfo[] subDirs = dirC.GetDirectories();
+            foreach (DirectoryInfo dir in subDirs) {
+                if ((dir.Attributes & FileAttributes.Hidden) != FileAttributes.Hidden) {
+                    TreeNode subDir = root.Nodes.Add(dir.Name);
+                    subDir.ImageIndex = 1;
+                    subDir.SelectedImageIndex = 1;
+                }
+            }
+            FileInfo[] fichs = dirC.GetFiles();
+            foreach (FileInfo fich in fichs) {
+                if ((fich.Attributes & FileAttributes.Hidden) != FileAttributes.Hidden) {
+                    TreeNode subDir = root.Nodes.Add(fich.Name);
+                    subDir.ImageIndex = 2;
+                    subDir.SelectedImageIndex = 2;
+                }
+            }
+            treeView1.ExpandAll();
+        }
+    }
+}
